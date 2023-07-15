777c7930fc4656df34f47cbd3cdad9dc8f894cc9
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-15 10:51:44 +0800

    update2
    please work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div>
+    <div className="box">
       <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
     </div>
   );
