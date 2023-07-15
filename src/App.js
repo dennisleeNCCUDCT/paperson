528b754ded19506d2fcea877d65674e70caf35ce
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-15 10:57:49 +0800

    fuck

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   return (
     <div className="box">
-      <canvas
-        style="display: block; width: 100%; height: 100%;"
-        data-engine="three.js r149"
-        width="375"
-        height="375"
-      >
-        <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
-      </canvas>
+      <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
     </div>
   );
 }
