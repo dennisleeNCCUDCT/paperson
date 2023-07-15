7689136450b6dcc1fd0b155ef44444ff8777bc9a
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-15 10:49:04 +0800

    update1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
+    <div>
+      <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
+    </div>
   );
 }
