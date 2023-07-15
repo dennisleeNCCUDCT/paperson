e7542d813cd37ba57f7822cc942c8afffff4ae77
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-15 13:00:08 +0800

    oooo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import styled from "styled-components";
 
 export default function App() {
   return (
-    <Wrapper>
-      <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
-    </Wrapper>
+    <Spline scene="https://prod.spline.design/P70WuUKssfdSpvOK/scene.splinecode" />
   );
 }
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  min-height: 720px;
-`;
